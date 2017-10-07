63b16666757f9e22aa17f61e226c5d040355afbc
Franklin Webber <franklin.webber@gmail.com>
2017-10-07 09:53:50 -0500

    Adds Habby, the happy Habitat
    This will report the IP and the time.
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- webapp/plan.sh
+++ webapp/plan.sh
@@ -6,16 +6,18 @@ pkg_license=('Apache-2.0')
 pkg_deps=(core/httpd)
 pkg_svc_user="root"
 pkg_svc_group="root"
+pkg_svc_run="httpd -DFOREGROUND -f $pkg_svc_config_path/httpd.conf"
+
 pkg_exports=(
   [port]=serverport
 )
 
 do_download() {
-  return 0
+  return 0;
 }
 
 do_build() {
-  return 0
+  return 0;
 }
 
 do_install() {
